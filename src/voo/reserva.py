fc3ce6e5d2a246ccd626a7c5422b839cdc1b7160
Erisvaldo <erisvaldoapps@gmail.com>
2023-12-27 11:33:02 -0300

    docstring

--- src/voo/reserva.py
+++ src/voo/reserva.py
@@ -2,7 +2,13 @@ class Reserva:
     """Classe que representa uma reserva.
     Atributos:
         passageiro (Passageiro): passageiro que fez a reserva
-        voo (Voo): voo da reserva"""
+        voo (Voo): voo da reserva
+        assento (int): assento da reserva
+    
+    Métodos:
+        get_passageiro: retorna o passageiro da reserva
+        get_voo: retorna o voo da reserva
+        get_assento: retorna o assento da reserva"""
     def __init__(self, passageiro, voo, assento):
         self.__passageiro = passageiro
         self.__voo = voo
